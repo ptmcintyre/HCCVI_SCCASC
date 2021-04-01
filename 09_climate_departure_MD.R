23e11e1d8c8c451439ed5b2f02c5aeee5dc45ee0
Patrick McIntyre <Patrick_McIntyre@nature.serve>
2021-04-01 14:41:06 -0400

    moving scripts 09 for climate departure(Mahalanobis distance) and final combined exposure to main scripting area

---
+++ 09_climate_departure_MD.R
@@ -0,0 +1,163 @@
+###working as of 3/19/2020 PJM, with a kludge for getting sigma transformation of MD
+## couldn't calculate in the function, so reading the results of the raw MD raster in and converting
+## getting percentile of chi-square based on 2df (2 pca axis), then getting quantile with 1df, per Mahoney paper.
+
+
+library(ecoclim)
+library(raster)
+library(doParallel)
+library(tidyverse)
+library(here)
+library(adehabitatLT)
+#library(caret)
+
+#gc()
+
+# load baseline TopoPrism data
+b <- parseMetadata(here("biovars/biovars_by_year_multiband/baseline"))
+#b[-grep("rcp85", b$path), ]#drop rcp85
+b <- dplyr::arrange(b, year)
+b <- b[b$year <= 2005,]
+baseline <- b
+
+# load  normals for comparing time period and emissions scenario
+#r <- parseMetadata("I:/climate_data/TopoWx/v2014/derived/normals/biovars/rasters_means", pattern=".tif")
+r <- parseMetadata(here("biovars/future_85"), pattern=".tif")
+#r <- r[grepl("1981_2014", r$path),]
+vars <- r$variable
+recent <- motleyStack(r$path)
+names(recent) <- vars
+
+# load veg data DO WE WANT BPS or EVT?
+veg_rasters<- list.files(here("system_distributions/MACA_rasters"), pattern=".tif")
+veggies <- raster::stack(here("system_distributions/MACA_rasters", veg_rasters))
+
+## filter out cnmx types
+#cnmx <- read.csv("I:/projects/DOCE/workspace/auer/identifying_cnmx_types/cnmx_types.csv")
+#cnmx <- as.character(cnmx$type)
+#veggies <- subset(veggies, names(veggies)[!names(veggies) %in% cnmx])
+
+# load variable importance data
+imp <- read.csv(here("type_specific_modeling/variable_selection/variable_importance.csv"), stringsAsFactors=F)
+imp <- imp[imp$rank >= 14,]
+
+
+novelty <- function(x, ...){
+      
+      # these lines are redundant, but it's faster since most cells are is.na(x[1])
+      if(is.na(x[1])) return(c(NA, NA))
+      if(any(is.na(x))) return(c(NA, NA))
+      
+      # unpack vector into matrix
+      x <- x[2:length(x)]
+      m <- matrix(x, ncol=6, byrow=T)
+      bl <- m[2:nrow(m),]
+      
+      # eliminate zero-variance variables
+      keep <- rep(T, 6)
+      for(i in 1:6) keep[i] <- sd(bl[,i]) != 0
+      keep <- which(keep)
+      bl <- bl[, keep]
+      rc <- matrix(m[1, keep], nrow=1)
+      
+      # reduce to first 2 principal components
+      pc <- prcomp(bl, scale.=T)
+      pcx <- pc$x[,1:2]
+      pcy <- predict(pc, rc)[,1:2]
+      
+      # mahalanobis distance
+      means <- colMeans(pcx)
+      cvm <- cov(pcx)
+      mdx <- mahalanobis(pcx, means, cvm)
+      mdy <- mahalanobis(pcy, means, cvm)
+      mdp <- mean(mdy >= mdx)
+      
+      c(mdy, mdp)
+}
+
+#type<-"Great_Basin_Pinyon_Juniper_Woodland"
+mahal <- function(type, overwrite=F){
+      
+      outfile<- paste0(here("type_specific_modeling/climate_departure/bl_fut_85"),"/", type, ".tif")
+      if(!overwrite & file.exists(outfile)){
+            message("skipping")
+            return("skipping")
+            }
+      message(type)
+      
+      start <- Sys.time()
+      
+      ### prep veg data
+      veg <- subset(veggies, type)
+      veg <- trimFast(extend(veg, 2))
+      names(veg) <- "veg"
+      
+      ### prep climate data
+      
+      # restrict analysis to focal vars
+      vars_used <- imp$var[imp$type == gsub("_", " ", type)][1:6]
+     
+      
+      b <- lapply(baseline$path, stack) %>%
+            lapply(function(x){
+                  names(x) <- paste0("bio", 1:19)
+                  x}) %>%
+            lapply(subset, subset=vars_used) %>%
+            stack()
+
+      # require(doParallel)
+      # cl <- makeCluster(detectCores() - 1)
+      # registerDoParallel(cl)
+      # b <- foreach(x = baseline$path, .packages=c("raster")) %dopar% {
+      #       x <- stack(x)
+      #       names(x) <- paste0("bio", 1:19)
+      #       crop(subset(x, vars_used), veg)
+      # }
+      # stopCluster(cl)
+      # b <- stack(b)
+      
+      #PJM: reducing to 4 variables
+      r <- subset(recent, vars_used) %>%
+            crop(b) %>% extend(b)
+      ve <- extend(veg, r) %>% 
+            crop(r) %>%
+            mask(r[[1]])
+      clim <- stack(ve, r, b)
+      
+      beginCluster(15)
+      md <- clusterR(clim, calc, args=list(fun=novelty), m = 100)
+      md <- crop(md, veg)
+      endCluster()
+      
+      # md <- calc(clim, function(x){
+      #       y <- try(novelty(x))
+      #       if(class(y) == "try-error") browser()
+      #       y
+      #       })
+      
+      writeRaster(md, outfile, overwrite=T)
+      
+      message(Sys.time() - start)
+      return("complete")
+}
+
+
+#types <- sample(names(veggies), length(names(veggies)))
+types<-names(veggies)
+v <- purrr::map_chr(types, possibly(mahal, "fail"))
+
+#read in test raster
+md.rasters <- parseMetadata(here("type_specific_modeling/climate_departure/bl_fut_85"),pattern=".tif")
+i=5
+
+names(md.rasters)
+for(i in 1:length(md.rasters)){
+  veg.md<-raster(md.rasters[[i]][1])
+  veg.md[!is.na(veg.md)]<-pchi(veg.md[!is.na(veg.md)], 2)
+  veg.md[!is.na(veg.md)]<-qchi(veg.md[!is.na(veg.md)], 1)
+  writeRaster(veg.md, paste0(here("type_specific_modeling/climate_departure/bl_fut_85"),"/", names(veg.md), "_sigma.tif"), fomrat="GTiff", overwrite=T)
+  
+}
+
+
+stopCluster(cl)
---
+++ 11_NEEDS_UPDATING_final_combined_exposure_index.R
@@ -0,0 +1,166 @@
+
+# This script combines task 1 typicality exposure and task 2
+# bioclimatic niche exposure into a final exposure index
+# layer for each veg type.
+
+
+library(ecoclim)
+library(raster)
+library(doParallel)
+
+
+# typicality and niche raster inputs
+d1 <- list.files("I:/projects/BLM/Production/type_specific/trend_typicality/historic_conus/rasters", full.names=T)
+d2 <- list.files("I:/projects/BLM/Production/type_specific/niche_models/historic_conus/rasters_delta", full.names=T)
+d1 <- d1[!grepl(".aux|.ovr", d1)]
+d2 <- d2[!grepl(".aux|.ovr", d2)]
+veggies <- substr(basename(d1), regexpr("_2014_", basename(d1)) + 6, nchar(basename(d1))-4)
+
+# veg distribution data
+# load veg data
+paths <- parseMetadata("I:/projects/BLM/Production/type_specific/veg_distributions/PRISM_800m/rasters", pattern=".tif")
+paths <- paths[!grepl(".aux|.ovr", paths)]
+distrib <- stack(paths)
+names(distrib) <- sub(".tif", "", basename(paths))
+
+# select CONUS types
+cnmx <- c("Sonora_Mojave_Creosotebush_White_Bursage_Desert_Scrub",
+          "Sonoran_Paloverde_Mixed_Cacti_Desert_Scrub",
+          "Apacherian_Chihuahuan_SemiDesert_Grassland_and_Steppe",
+          "Chihuahuan_Creosotebush_Desert_Scrub",
+          "Chihuahuan_Mixed_Desert_and_Thornscrub",
+          "Northwestern_Great_Plains_Mixedgrass_Prairie", 
+          "Rocky_Mountain_Foothill_Limber_Pine_Juniper_Woodland",
+          "Madrean_Pinyon_Juniper_Woodland",
+          "Northern_Rocky_Mountain_Subalpine_Woodland_and_Parkland")
+distrib <- subset(distrib, names(distrib)[!names(distrib) %in% cnmx])
+
+
+# cluster stetup
+cpus <- 12
+cl <- makeCluster(cpus)
+registerDoParallel(cl)
+
+# loop
+r <- foreach(type=veggies,
+             .packages=c("raster", "ecoclim", "ggplot2", "viridis")) %dopar% {
+                   
+                   # open input rasters
+                   s <- stack(d1[grepl(type, d1)],
+                              d2[grepl(type, d2)])
+                   names(s) <- c("t1r", "t2r")
+                   
+                   # rescale inputs from 0 (good) to .5 (bad) exposure range
+                   s$t1 <- (1 - s$t1r) / 2
+                   s$t2 <- (-1 * s$t2r + 1) / 4
+                   
+                   # save 2d sum
+                   index <- s$t1 + s$t2
+                   try(writeRaster(index,
+                                   paste0("I:/projects/BLM/Production/type_specific/combined_exposure/historic_conus/rasters/historic_exposure_index_", type), 
+                                   format="GTiff", overwrite=T))
+                   
+                   typename <- gsub(".tif", "", type)
+                   db <- subset(distrib, typename)
+                   db <- crop(db, s)
+                   s <- mask(s, db)
+                   index <- mask(index, s[[1]])
+                   s <- stack(s, index, db)
+                   names(s) <- c("t1r", "t2r", "t1", "t2", "index", "veg")
+                   
+                   try(writeRaster(s[[1:5]],
+                                   paste0("I:/projects/BLM/Production/type_specific/combined_exposure/historic_conus/rasters_clipped/historic_exposure_components_", type), 
+                                   format="GTiff", overwrite=T))
+                   
+                   d <- as.data.frame(rasterToPoints(s))
+                   d <- d[!is.na(d$veg),]
+                   
+                   # scatterplot
+                   p <- ggplot(d[sample(nrow(d), min(nrow(d), 10000)),], aes(t1, t2, color=index)) +
+                         geom_vline(xintercept=c(0,.5), color="gray") +
+                         geom_hline(yintercept=c(0,.25,.5), color="gray") +
+                         geom_point(size=2, alpha=.3) +
+                         geom_density2d(color="black") +
+                         geom_abline(slope=-1, intercept=.25) +
+                         geom_abline(slope=-1, intercept=.5) +
+                         geom_abline(slope=-1, intercept=.75) +
+                         xlim(0,.5) + ylim(0,.5) +
+                         coord_fixed() +
+                         scale_color_viridis(limits=c(0,1)) +
+                         whiteness() +
+                         labs(x = "Task 1 exposure (rescaled)",
+                              y = "Task 2 exposure (rescaled)",
+                              color = "combined\nindex",
+                              title = paste0("Exposure index for ", gsub("_", " ", typename)))
+                   ggsave(paste0("I:/projects/BLM/Production/type_specific/combined_exposure/historic_conus/charts/historic_exposure_scatter_", typename, ".png"),
+                          width=8, height=6)
+                   
+                   
+                   # prep state boundary data
+                   states <- rgdal::readOGR("I:/projects/BLM/Workspace/kling/shapefiles", layer="US_states")
+                   ext <- extent(s)
+                   ext <- as(ext, "SpatialPolygons")
+                   proj4string(ext) <- CRS(proj4string(states))
+                   states <- rgeos::gIntersection(states, ext, byid=T)
+                   states.points <- fortify(states)
+                   states.points$region <- "states"
+                   states.points$polygon <- as.numeric(states.points$piece) + 1000
+                   
+                   # map: index
+                   p <- ggplot() +
+                         geom_polygon(data=states.points[states.points$piece==1,], aes(x=long, y=lat, group=id),
+                                      color="white", fill="gray90", size=.6) +
+                         geom_raster(data=d, aes(x, y, fill=index)) +
+                         geom_path(data=states.points[states.points$piece==1,], aes(x=long, y=lat, group=id),
+                                   color="white", fill=NA, size=.6) +
+                         coord_fixed(ratio=1.2) +
+                         scale_fill_viridis(limits=c(0,1)) +
+                         whiteness() +
+                         labs(fill = "Final\nindex",
+                              title = paste0("Exposure index for ", gsub("_", " ", typename))) +
+                         theme(axis.title=element_blank(), axis.text=element_blank(), 
+                               axis.ticks=element_blank(), panel.grid=element_blank())
+                   ggsave(paste0("I:/projects/BLM/Production/type_specific/combined_exposure/historic_conus/charts/historic_exposure_map_", typename, ".png"),
+                          width=8, height=6)
+                   
+                   # map: task 1
+                   p <- ggplot() +
+                         geom_polygon(data=states.points[states.points$piece==1,], aes(x=long, y=lat, group=id),
+                                      color="white", fill="gray90", size=.6) +
+                         geom_raster(data=d, aes(x, y, fill=t1r)) +
+                         geom_path(data=states.points[states.points$piece==1,], aes(x=long, y=lat, group=id),
+                                   color="white", fill=NA, size=.6) +
+                         coord_fixed(ratio=1.2) +
+                         scale_fill_gradientn(colours=rev(viridis_pal()(256)), limits=c(0,1)) +
+                         whiteness() +
+                         labs(fill = "Task 1\nindex",
+                              title = paste0("Task 1 typicality for ", gsub("_", " ", typename))) +
+                         theme(axis.title=element_blank(), axis.text=element_blank(), 
+                               axis.ticks=element_blank(), panel.grid=element_blank())
+                   ggsave(paste0("I:/projects/BLM/Production/type_specific/combined_exposure/historic_conus/charts/historic_exposure_map_t1_", typename, ".png"),
+                          width=8, height=6)
+                   
+                   # map: task 2
+                   orangepurple <- c('#b35806','#f1a340','#fee0b6','#f7f7f7','#d8daeb','#998ec3','#542788')
+                   p <- ggplot() +
+                         geom_polygon(data=states.points[states.points$piece==1,], aes(x=long, y=lat, group=id),
+                                      color="white", fill="gray90", size=.6) +
+                         geom_raster(data=d, aes(x, y, fill=t2r)) +
+                         geom_path(data=states.points[states.points$piece==1,], aes(x=long, y=lat, group=id),
+                                   color="white", fill=NA, size=.6) +
+                         coord_fixed(ratio=1.2) +
+                         scale_fill_gradientn(colours=orangepurple,
+                                              limits=c(-1,1)) +
+                         whiteness() +
+                         labs(fill = "Task 2\nindex",
+                              title = paste0("Task 2 niche trend for ", gsub("_", " ", typename))) +
+                         theme(axis.title=element_blank(), axis.text=element_blank(), 
+                               axis.ticks=element_blank(), panel.grid=element_blank())
+                   ggsave(paste0("I:/projects/BLM/Production/type_specific/combined_exposure/historic_conus/charts/historic_exposure_map_t2_", typename, ".png"),
+                          width=8, height=6)
+                   
+                   d$veg <- typename
+                   d
+             }
+
+stopCluster(cl)

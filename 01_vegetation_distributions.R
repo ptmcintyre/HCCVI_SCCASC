bbfd410a32fa23d60fa3cf3b2db6b71e92ab6ef9
Patrick McIntyre <Patrick_McIntyre@nature.serve>
2022-01-11 18:18:02 +0000

    updating from CEMML to SCCASC file strutcture

--- 01_vegetation_distributions.R
+++ 01_vegetation_distributions.R
@@ -32,27 +32,34 @@ template<-rast(historic.biovars[1])  #using new terra package
 #CEMML_systems_BPS<-raster(here("system_distributions/IVC_BPS_CEMML_v846", "IVC_BPS_CEMML_v846.tif")) #clipped version
 #CEMML_systems_BPS<-rast("S:/Projects/SCCASC_HCCVI/SCCASC_GIS/NorthAmerica_IVC_Ecosystems_potential_NatureServe_v846.tif")
 #CEMML_systems_BPS<-raster("S:/Projects/SCCASC_HCCVI/SCCASC_GIS/NorthAmerica_IVC_BPS846_wgs84.tif")
-CEMML_systems_BPS2<-rast("S:/Projects/SCCASC_HCCVI/SCCASC_GIS/NorthAmerica_IVC_BPS846_wgs84.tif")
-activeCat(CEMML_systems_BPS2, layer=1)
-setCats(CEMML_systems_BPS2, layer=1,cats(CEMML_systems_BPS2)[1]$Value)
+#CEMML_systems_BPS<-rast("S:/Projects/SCCASC_HCCVI/SCCASC_GIS/NorthAmerica_IVC_BPS846_wgs84.tif")
+#activeCat(CEMML_systems_BPS, layer=1)
+#setCats(CEMML_systems_BPS, layer=1,cats(CEMML_systems_BPS)[1]$Value)
 
 
 detectCores()
-cpus <- 5
+cpus <- 2
 cl <- makeCluster(cpus)
 registerDoParallel(cl)
 i=1
 foreach(i=1:length(map_values)) %dopar% {
-  .libPaths("C:/Users/patrick_mcintyre/Documents/R/win-library/4.1")
-  library(raster)
+  #.libPaths("C:/Users/Patrick_McIntyre/Documents/R/win-library/4.1")
+  .libPaths("C:/Users/patrick_mcintyre/Documents/R/win-library/4.1")  
+  #library(raster)
   library(here)
+  library(terra)
+  CEMML_systems_BPS<-rast("S:/Projects/SCCASC_HCCVI/SCCASC_GIS/NorthAmerica_IVC_BPS846_wgs84.tif")
+  activeCat(CEMML_systems_BPS, layer=1)
+  setCats(CEMML_systems_BPS, layer=1,cats(CEMML_systems_BPS)[1]$Value)
   focal_system<-CEMML_systems_BPS
   focal_system<-focal_system %in% map_values[i]
   #focal_system[focal_system!=map_values[i]]<-NA
   #veg <- reclassify(veg, rcl=c(-1,1,NA,  1,Inf,1), right=F)
   select_system_info<-subset(target_systems, target_systems$NS.Map.Value.code==map_values[i])
-  writeRaster(focal_system, filename=here("system_distributions/system_rasters_raw", paste(select_system_info$system_name[1],".grd", sep="")), overwrite=T)
+  writeRaster(focal_system, filename=here("system_distributions/system_rasters_raw/", paste(select_system_info$system_name[1],".tif", sep="")), overwrite=T)
 }
 
 stopCluster(cl)
 
+
+?foreach

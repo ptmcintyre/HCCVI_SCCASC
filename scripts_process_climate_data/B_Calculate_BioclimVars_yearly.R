bcecd53e08563f6339037963322b2d993bcb4188
Patrick McIntyre <Patrick_McIntyre@nature.serve>
2022-01-20 18:39:24 +0000

    updates with annotation and providing code to create directories for outputs.

--- scripts_process_climate_data/B_Calculate_BioclimVars_yearly.R
+++ scripts_process_climate_data/B_Calculate_BioclimVars_yearly.R
@@ -1,65 +1,78 @@
-###
+###This script uses the ecoclim package to convert monthl precip/tmin/tmax
+#to bioclim variables 
+
+##To install ecoclim 02/17/2021 run code below
+#install.packages("remotes")
+#remotes::install_github("matthewkling/ecoclim") 
+
+
+rm(list=ls())
+gc()
 
 
 library(raster)
 library(ecoclim)
 
-##To install ecoclim 02/17/2021
-#install.packages("remotes")
-#remotes::install_github("matthewkling/ecoclim") 
+#USER INPUTS#########################
+
+#UPDATE NAME OF GCM for addding to output file names
+GCM<- "CCSM4"   #enter name of GCM to include with file
+
+##concatenate GCM name with other info, don't change
+historic_name<-paste0("LOCA_", GCM,"_historical")
+future_name<-paste0("LOCA_", GCM,"_rcp85")
+
+##define output base location, will add on GCM name
+#all files from a GCM for both historic and future will go to a single folder)
+base_out<-"S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/biovars_yearWGS84/"
+output_loc<- paste0(base_out, GCM)
+dir.create(output_loc)
 
 
+#update location of historic and future (rcp85) monthly climate files. Should have just one GCM worths of files at a time here
+#files should be precip/tmin/tmax for all years in historic(1950-2005) and future (2006-2100) in separate folders (could be updated to single)
 
-#years<-seq(1950,2005, 1) #for historic
-years<-seq(2006,2100, 1) #for future
-#base_name<-"LOCA_CCSM4_historical"
-base_name<-"LOCA_CCSM4_rcp85"
+historic_monthly<-"S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/monthly_vals_year/test/historic"
+future_monthly<-"S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/monthly_vals_year/test/future"
 
-i=1
-for(i in 1:length(years)) {
-#clim.files <- list.files("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/monthly_vals_year/historic", pattern=paste0(years[i]), full.names =T)
-clim.files <- list.files("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/monthly_vals_year/rcp85", pattern=paste0(years[i]), full.names =T)
-clim.files<-clim.files[c(1,5,6,7,8,9,10,11,12,2,3,4,25,29,30,31,32,33,34,35,36,26,27,28,13,17,18,19,20,21,22,23,24,14,15,16)]
-clim.year<-stack(clim.files)
-k <- calc(clim.year, fun=function(x)biovariables(x))
+# shouldn't change- define years of interest for all historic and future years
+historic_years<-seq(1950,2005, 1)
+future_years<-seq(2006,2100, 1)
+
+###############################################
+#loop through historic data
+
+i=2
+j=1
+for(i in 2:length(historic_years)) {
+  #select a single year of files
+  clim.files <- list.files(historic_monthly, pattern=paste0(historic_years[i]), full.names =T)
+  #reorganize files into correct order, should be precip1_12,tmax1_12, tmin1_12
+  clim.files<-clim.files[c(1,5,6,7,8,9,10,11,12,2,3,4,13,17,18,19,20,21,22,23,24,14,15,16,25,29,30,31,32,33,34,35,36,26,27,28)]
+  clim.year<-stack(clim.files)  #stack climate files to brick
+  k <- calc(clim.year, fun=function(x)biovariables(x))  #apply biovariables function
+  #output files
   for (j in 1:19){
-    writeRaster(k[[j]], paste0("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/biovars_year/", 
-                          base_name,"_", years[i], "_", "bio",j, ".tif"), "GTiff", overwrite=TRUE)
-    }
-}
-
-years<-seq(1950,2005, 1) #for historic
-base_name<-"LOCA_CCSM4_historical"
-
-i=1
-for(i in 1:length(years)) {
-  clim.files <- list.files("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/monthly_vals_year/historic", pattern=paste0(years[i]), full.names =T)
-  #clim.files <- list.files("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/monthly_vals_year/rcp85", pattern=paste0(years[i]), full.names =T)
-  clim.files<-clim.files[c(1,5,6,7,8,9,10,11,12,2,3,4,25,29,30,31,32,33,34,35,36,26,27,28,13,17,18,19,20,21,22,23,24,14,15,16)]
-  clim.year<-stack(clim.files)
-  k <- calc(clim.year, fun=function(x)biovariables(x))
-  for (j in 1:19){
-    writeRaster(k[[j]], paste0("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/biovars_year/", 
-                               base_name,"_", years[i], "_", "bio",j, ".tif"), "GTiff", overwrite=TRUE)
+    writeRaster(k[[j]], paste0(output_loc, "/", historic_name,"_", historic_years[i], "_", "bio",j, ".tif"), "GTiff", overwrite=TRUE)
   }
 }
+
+################################################
+#loop through future data
+i=2
+j=1
+for(i in 2:length(future_years)) {
+  #select a single year of files
+  clim.files <- list.files(future_monthly, pattern=paste0(future_years[i]), full.names =T)
+  #reorganize files into correct order, should be precip1_12,tmax1_12, tmin1_12
+  clim.files<-clim.files[c(1,5,6,7,8,9,10,11,12,2,3,4,13,17,18,19,20,21,22,23,24,14,15,16,25,29,30,31,32,33,34,35,36,26,27,28)]
+  clim.year<-stack(clim.files)  #stack climate files to brick
+  k <- calc(clim.year, fun=function(x)biovariables(x))  #apply biovariables function
+  #output files
+  for (j in 1:19){
+    writeRaster(k[[j]], paste0(output_loc, "/", future_name,"_", future_years[i], "_", "bio",j, ".tif"), "GTiff", overwrite=TRUE)
+  }
+}
+
 rm()
 gc()
-
-##comparing extentents (seeing what stacks)
-# clim.files<-clim.files[c(1,5,6,7,8,9,10,11,12,2,3,4,25,29,30,31,32,33,34,35,36,26,27,28,13,17,18,19,20,21,22,23,24,14,15,16)]
-# clim.year<-stack(clim.files)
-# 
-# 
-# clim.files <- list.files("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/monthly_vals_year/rcp85", pattern=paste0(years[i]), full.names =T)
-# clim.files<-clim.files[c(1,5,6,7,8,9,10,11,12,2,3,4)]
-# clim.files<-clim.files[c(25,29,30,31,32,33,34,35,36,26,27,28)]
-# 
-# clim.files<-clim.files[c(13,17,18,19,20,21,22,23,24,14,15,16)]
-# clim.year<-stack(clim.files)
-# clim.year
-# 
-# 
-# 26,27,28,13,17,18,19,20,21,22,23,24
-
-
--- scripts_process_climate_data/C_biovars_calculate_normals.R
+++ scripts_process_climate_data/C_biovars_calculate_normals.R
@@ -1,79 +1,96 @@
+#this file takes yearly bioclim values and averages them accross years 
+#to create averages for target periods (e.g. 1950-1980, 2035-2075)
+rm(list=ls())
+gc()
+
 library(terra)
 library(raster)
+################################################################################
+#USER INPUTS
 
+#UPDATE NAME OF GCM for addding to output file names
+GCM<- "CCSM4"   #enter name of GCM to include with file
 
-bio_num<-seq(1,19,1)
-bio_names<-paste0("bio", bio_num, ".tif")
-#historical
+##concatenate GCM name with other info, don't change
+historic_name<-paste0("LOCA_", GCM,"_historical")
+future_name<-paste0("LOCA_", GCM,"_rcp85")
 
-i=1
+#####define inputs- location for all biovars by year from 1950-2100 for a single GCM
+biovars_yearly<-"S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/biovars_yearWGS84"
+#list.files(biovars_yearly)# check that location is correct
 
-for (i in 1:19){
-bio.yearly<-list.files("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/biovars_yearWGS84", pattern=bio_names[i], full.names = T)
-bio.hist<-list.files("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/biovars_yearWGS84", pattern="historical", full.names = T)
-bio_years<-bio.yearly[bio.yearly%in%bio.hist]
-bio_stack<-rast(c(bio_years[1:31]))
-bio_stack_mean<-mean(bio_stack)
+###define output locations
+historic_out<-"S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/bio_vars_normals/historic/"
+historic_out<- paste0(historic_out, GCM)
+dir.create(historic_out)
 
-writeRaster(bio_stack_mean, paste0("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/bio_vars_normals/historic/", 
-                      "LOCA_CCSM4_historical_1950_1980_", bio_names[i]), overwrite=TRUE)
+future_out<-"S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/bio_vars_normals/future/"
+future_out<-paste0(future_out, GCM)
+dir.create(future_out)
 
-
-}
-
-
-i=1
-##below does RCP85 for 2015-2045
-for (i in 1:19){
-  bio.yearly<-list.files("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/biovars_yearWGS84", pattern=bio_names[i], full.names = T)
-  bio.hist<-list.files("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/biovars_yearWGS84", pattern="rcp85", full.names = T)
-  bio_years<-bio.yearly[bio.yearly%in%bio.hist]
-  bio_stack<-rast(c(bio_years[10:41])) #
-  bio_stack_mean<-mean(bio_stack)
-  
-  writeRaster(bio_stack_mean, paste0("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/bio_vars_normals/future/", 
-                                     "LOCA_CCSM4_RCP85_2015_2045_", bio_names[i]), overwrite=TRUE)
-  
-  
-}
-
-
-
-
-i=1
-##below does RCP85 for 2015-2045
-for (i in 1:19){
-  bio.yearly<-list.files("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/biovars_yearWGS84", pattern=bio_names[i], full.names = T)
-  bio.hist<-list.files("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/biovars_yearWGS84", pattern="rcp85", full.names = T)
-  bio_years<-bio.yearly[bio.yearly%in%bio.hist]
-  bio_stack<-rast(c(bio_years[41:72])) #
-  bio_stack_mean<-mean(bio_stack)
-  
-  writeRaster(bio_stack_mean, paste0("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/bio_vars_normals/future/", 
-                                     "LOCA_CCSM4_RCP85_2045_2075_", bio_names[i]), overwrite=TRUE)
-  
-  
-}
-
-
-
-
-
-bio_stack[31]
-
-plot(bio_stack_mean)
-
-bio_years
-
-junk<-
-years<-seq(1950, 2005, 1)
-years<-paste0(years,"_")
-
-list.files
-?list.files()
-#make normals
-bio.yearly<-list.files("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/biovars_yearWGS84")
-bio.yearly
+###define bioclim variables by simple name
+#and concatenate with .tif to filter files
 
 bio_num<-seq(1,19,1)
 bio_names<-paste0("bio", bio_num, ".tif")
+
+####################################################################################
+#loop through historical 
+
+i=1
+
+for (i in 1:19){
+#pull out files with bio1 variable
+bio.yearly<-list.files(biovars_yearly, pattern=bio_names[i], full.names = T)
+#pull out historical files
+bio.target<-list.files("S:/Projects/SCCASC_HCCVI/HCCVI_SCCASC_R_Project/process_initial_climate_data/biovars_yearWGS84", pattern="historical", full.names = T)
+bio_years<-bio.yearly[bio.yearly%in%bio.target]
+bio_stack<-rast(c(bio_years[1:31])) ###1:31 corresponds to 1950-1980
+#names(bio_stack)# can check that years are correct
+bio_stack_mean<-mean(bio_stack) #average accross years
+
+writeRaster(bio_stack_mean, paste0(historic_out,"/", historic_name, "_1950_1980_", bio_names[i]), overwrite=TRUE)
+
+}
+
+####################################################################################
+#loop through near future (2015-2045) 
+
+i=1
+
+for (i in 1:19){
+  #pull out files with bio1 variable
+  bio.yearly<-list.files(biovars_yearly, pattern=bio_names[i], full.names = T)
+  #pull out future files
+  bio.target<-list.files(biovars_yearly, pattern="rcp85", full.names = T)
+  bio_years<-bio.yearly[bio.yearly%in%bio.target]
+  bio_stack<-rast(c(bio_years[10:40])) ###1:31 corresponds to 2015-2045
+  #names(bio_stack)# can check that years are correct
+  bio_stack_mean<-mean(bio_stack) #average accross years
+  
+  writeRaster(bio_stack_mean, paste0(future_out,"/", future_name, "_2015_2045_", bio_names[i]), overwrite=TRUE)
+  
+}
+
+####################################################################################
+#loop through later future (2045-2075) 
+
+i=1
+
+for (i in 1:19){
+  #pull out files with bio1 variable
+  bio.yearly<-list.files(biovars_yearly, pattern=bio_names[i], full.names = T)
+  #pull out future files
+  bio.target<-list.files(biovars_yearly, pattern="rcp85", full.names = T)
+  bio_years<-bio.yearly[bio.yearly%in%bio.target]
+  bio_stack<-rast(c(bio_years[40:70])) ###corresponds to 2045-20175
+  #names(bio_stack)# can check that years are correct
+  bio_stack_mean<-mean(bio_stack) #average accross years
+  
+  writeRaster(bio_stack_mean, paste0(future_out,"/", future_name, "_2045_2075_", bio_names[i]), overwrite=TRUE)
+  
+}
+rm(list=ls())
+gc()
+
+
